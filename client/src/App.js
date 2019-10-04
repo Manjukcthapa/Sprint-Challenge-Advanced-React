2f4fa0767f17e4308bf9c6bd8d5afae3051f72f5
Manju KC <kcmanjuthapa@gmail.com>
2019-10-04 11:18:23 -0400

    added constructor function inside app function

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      players:[]
+    }
+  }
 
   render(){
     return(
