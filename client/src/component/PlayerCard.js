1cde24324a740c73cb5731f1ec9e671a0ec637c4
Manju KC <kcmanjuthapa@gmail.com>
2019-10-04 11:35:05 -0400

    imported PlayerCard component to the PlayerList file

--- client/src/component/PlayerCard.js
+++ client/src/component/PlayerCard.js
@@ -2,9 +2,9 @@ import React from "react";
 import { tsPropertySignature } from "@babel/types";
 
 export default function PlayerCard(props) {
-    return(
-        <div>
-            <p>{props.member.name}</p>
-        </div>
-    )
+  return (
+    <div>
+      <p>{props.member.name}</p>
+    </div>
+  );
 }
---
+++ client/src/component/PlayerList.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PlayerCard from "./PlayerCard"
+
+export default function PlayerList() {
+  return(
+  <div>
+<PlayerCard/>
+  </div>
+  );
+}
