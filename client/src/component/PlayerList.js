56a6e46c5afab3e511b4ade59e6268e1b77b2204
Manju KC <kcmanjuthapa@gmail.com>
2019-10-04 11:38:04 -0400

    maped the data

--- client/src/component/PlayerList.js
+++ client/src/component/PlayerList.js
@@ -1,10 +1,13 @@
 import React from "react";
 import PlayerCard from "./PlayerCard"
+import { tsPropertySignature } from "@babel/types";
 
-export default function PlayerList() {
+export default function PlayerList(props) {
   return(
   <div>
-<PlayerCard/>
+ {props.players.map(member => {
+     return<PlayerCard member={member.id} member={member}/>
+ })}     
   </div>
   );
 }
